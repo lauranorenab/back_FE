6acfa34434b00f1705fca3dc1143a042ccfe025a
JoseGomez14 <jose.gomez14@udea.edu.co>
2024-03-17 16:29:58 -0500

    feat: :sparkles: Add entity DTOs
    - Flight
    - Scale
    - Airplane Model
    - Airport
    
    Co-authored-by: daniel-lujan <daniel.lujan@udea.edu.co>
    Co-authored-by: anavegaa <ana.vega@udea.edu.co>

--- src/main/java/com/udea/sitas/model/DTO/AirplaneModelDTO.java
+++ src/main/java/com/udea/sitas/model/DTO/AirplaneModelDTO.java
@@ -1,4 +1,20 @@
 package com.udea.sitas.model.DTO;
 
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+@AllArgsConstructor
 public class AirplaneModelDTO {
+    @NotEmpty
+    private String id;
+
+    private String family;
+
+    private int capacity;
+
+    private long cargoCapacity;
 }
--- src/main/java/com/udea/sitas/model/DTO/AirportDTO.java
+++ src/main/java/com/udea/sitas/model/DTO/AirportDTO.java
@@ -1,4 +1,24 @@
 package com.udea.sitas.model.DTO;
 
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@RequiredArgsConstructor
 public class AirportDTO {
+    @NotEmpty
+    private String id;
+
+    private String name;
+
+    private String type;
+
+    private String city;
+
+    private String country;
+
+    private int runways;
 }
