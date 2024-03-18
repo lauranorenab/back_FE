e004bc2981dc4a3ca377d8a3bd79a5f11cbbba81
daniel-lujan <daniel.lujan@udea.edu.co>
2024-03-18 18:39:36 -0500

    feat: :sparkles: Add database setup scripts
    Co-authored-by: JoseGomez14 <jose.gomez14@udea.edu.co>

---
+++ init-scripts/populate.sql
@@ -0,0 +1,38 @@
+INSERT INTO
+  AirplaneModel (airplane_model, family, capacity, cargo_capacity)
+VALUES
+  ('A320-200', 'Airbus', 180, 20000);
+
+INSERT INTO
+  AirplaneModel (airplane_model, family, capacity, cargo_capacity)
+VALUES
+  ('A320-300', 'Airbus', 277, 45000);
+
+INSERT INTO
+  AirplaneModel (airplane_model, family, capacity, cargo_capacity)
+VALUES
+  ('A330-300', 'Airbus', 277, 45000);
+
+INSERT INTO
+  Airport (airport_code, name, type, city, country, runways)
+VALUES
+  (
+    'MDE',
+    'Jose Maria Cordova',
+    'International',
+    'Medellin',
+    'Colombia',
+    2
+  );
+
+INSERT INTO
+  Airport (airport_code, name, type, city, country, runways)
+VALUES
+  (
+    'BOG',
+    'El Dorado',
+    'International',
+    'Bogota',
+    'Colombia',
+    3
+  );
